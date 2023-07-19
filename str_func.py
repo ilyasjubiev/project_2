d1ec4b2a1baddfb29bfa605e086fe1b4f6b63097
ilyas <jubiev2017@gmail.com>
2023-07-19 05:34:33 +0300

    add str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def capital_letters(word):
+     """функция принимает буквы в нижнем регистре
+    и выводит буквы в верхнем регистре""" 
     return word.upper()
+
+def first_letter_capital(words):
+    """принимат слова и выдает
+     с заглавной буквы"""
+    return words.title()
+
